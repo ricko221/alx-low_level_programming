2d340fb95de814b5779207884bc08c40d173f34e
Fredrick Owiti <fotsola10@gmail.com>
2021-06-22 05:39:07 -0700

    holbertons header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character to stdout
+ * print_alphabet - prints the alphabet and returns a new line
+ * print_alphabet_x10 - prints alphabets ten times
+ * _islower - returns 1 if c is uppercase and 0 otherwise
+ * _isalpha - returns 1 if c is upper/lower case, 0 otherwise
+ * print_sign - returns 1 for positive, zero for 0, and -1 for negative
+ * _abs - computes absolute value of an integer
+ * print_last_digit _ print last digit
+ * add - add two ints, return sum
+ * jack-bauer - print all minutes of the day
+ * times_table - prints times table up to 9
+ * print_to_98 - prints all natural numbers to 98
+ * print_times_table - prints times table
+ **/
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
