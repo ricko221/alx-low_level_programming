0d041011a4dc2c708e7e2522aeb2ee9a807dc0c4
Fredrick Owiti <fotsola10@gmail.com>
2021-06-22 10:12:00 -0700

    natural integers fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
  * print_to_98 - function to print numbers
  * @n : integer value
  * Return: Always 0.
- */
+ **/
 void print_to_98(int n)
 {
 	int a;
@@ -16,6 +16,6 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 		for (a = n; a > 98; a--)
-			printf("%d, ", a);
+		 printf("%d, ", a);
 	printf("98\n");
 }
