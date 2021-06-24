e33294cb79676af2a273ee43d7d42e28a19d331e
Fredrick Owiti <fotsola10@gmail.com>
2021-06-24 07:09:18 -0700

    Headers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
-void positive_or_negatve(int i)
+int main(void)
 {
-  int i = 0;
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+return (0);
 }
+
