52d509afa52a7a63b718e5197646b152957c886a
Fredrick Owiti <fotsola10@gmail.com>
2021-06-24 02:18:49 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+  
+#include "holberton.h"
+
+/**
+ * mul - function to multiply
+ * @a : integer value
+ * @b : integer value
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
