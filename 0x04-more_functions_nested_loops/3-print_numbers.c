239ac8c5b8f2c3aaa1f1bb52c722925e88d479f6
Fredrick Owiti <fotsola10@gmail.com>
2021-06-24 02:22:33 -0700

    printing 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function to print numbers
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		_putchar(a + '0');
+	_putchar('\n');
+}
